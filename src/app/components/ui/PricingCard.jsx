ffb193aaf89e935ee29ad75ab26106867b37742f
Benjamin Batres <benbatres1212@gmail.com>
2026-04-04 16:52:44 -0700

    implemented animation to pricing section

--- src/app/components/ui/PricingCard.jsx
+++ src/app/components/ui/PricingCard.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+'use client'
+import React, { useEffect, useRef } from "react";
 import { GiCheckMark } from "react-icons/gi";
-
+import * as motion from "motion/react-client";
+import { useAnimation, useInView } from "motion/react";
 export default function PricingCard({ info }) {
+  const containerRef = useRef(null);
+    const isInView = useInView(containerRef);
+    const mainControls = useAnimation();
+  
+    useEffect(() => {
+      if (isInView) {
+        mainControls.start("visible");
+      }
+    }, [isInView]);
+  
   return (
-    <div className="border-2 rounded-[20px] py-12 md:py-15 px-10 flex flex-col items-center">
+    <motion.div
+    ref={containerRef}
+      animate={mainControls}
+      initial="hidden"
+      variants={{
+        hidden: { opacity: 0, y: 25 },
+        visible: {
+          opacity: 1,
+          y: 0,
+        },
+      }}
+      transition={{ delay: 0.7, duration: 0.7 }}
+     className="border-2 rounded-[20px] py-12 md:py-15 px-10 flex flex-col items-center">
       <div
         className="py-1.5 px-3.75 border-2 rounded-full font-bold uppercase mb-6"
         style={{ backgroundColor: info.bgColor }}
@@ -15,13 +39,13 @@ export default function PricingCard({ info }) {
       </h2>
       <p className="text-lg font-light mb-6">{info.description}</p>
       <div className="space-y-2">
-        {info.perks.map((perk) => (
-          <div className="flex gap-2">
+        {info.perks.map((perk, id) => (
+          <div className="flex gap-2" key={id}>
             <GiCheckMark className="mt-1 text-lg" />
             <div className="text-lg">{perk}</div>
           </div>
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 }
